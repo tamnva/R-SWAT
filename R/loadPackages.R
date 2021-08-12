bb7d2931f86b6f10e428097ebebd7082248b1945
nguyenta <tam.nguyen@ufz.de>
2021-08-12 14:54:30 +0200

    Update loadPackages.R

--- R/loadPackages.R
+++ R/loadPackages.R
@@ -1,29 +1,17 @@
 
 # Dependency packages
-  packages <- c('foreach', 
-                'parallel', 
-                'lhs', 
-                'raster', 
-                'rgdal', 
-                'plotly', 
-                'shinydashboard', 
-                'shinyFiles', 
-                'tippy', 
-                'excelR')
+  packages <- c('foreach', 'parallel', 'lhs', 'plotly', 'shinydashboard', 
+                'shinyFiles', 'tippy', 'excelR', 'shiny')
 
 # Install dependency packages if necessary
-  install.packages(repos = 'http://cran.us.r-project.org',
-                   setdiff(packages, 
-                           rownames(installed.packages())
-                           )
-                   ) 
+  install.packages(setdiff(packages,rownames(installed.packages())),
+                   repos = 'http://cran.us.r-project.org') 
 
 # Load dependency packages
+  library(shiny)
   library(foreach)
   library(lhs)
   library(parallel)
-  library(raster)
-  library(rgdal)
   library(plotly)
   library(shinydashboard)
   library(shinyFiles)
